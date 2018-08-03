f7e269ec103a72801c68662258d31c3ec336da09
Rajeev Ram <rajeevram@email.arizona.edu>
2018-08-03 11:47:08 -0500

    Working on first user story

---
+++ Yelp/Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Rajeev Ram on 8/3/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
